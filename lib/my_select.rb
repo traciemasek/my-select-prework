e76a663c999f635547c20c3b04e098dc466000e5
Tracie Masek <traciemasek@gmail.com>
2019-07-19 10:56:09 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,8 +1,12 @@
 def my_select(collection)
  i = 0 
+ selections = []
  
  while i < collection.length 
-   yield collection[i]
+   if yield collection[i]
+     selections << collection[1]
+   end
    i += 1 
  end
+ selections
 end
